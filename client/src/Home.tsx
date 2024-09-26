12972bcaf614774a9049f7f1e3b09925e4196091
Phil <philippe@rebeiro.ca>
2024-09-26 19:16:32 -0400

    Added text, inputs and map hardcode to home page

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,14 +1,18 @@
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+
 function Home() {
   return (
     <section className="flex-1 relative">
-      <div className="max-w-screen-xl mx-auto px-16 h-full flex items-center justify-around">
-        <div>
-          {/* form goes here */}
-          form
+      <div className="max-w-screen-xl mx-auto px-16 h-full flex items-center justify-around gap-4">
+        <div className="w-[300px] flex flex-col gap-4 items-end">
+          <div className="text-4xl font-bold">Ship anything with Vüber</div>
+           <Input placeholder="From" className=""></Input>
+           <Input placeholder="To" className=""></Input>
+           <Button className="">Get Quote</Button>
         </div>
         <div>
-          {/* map goes here */}
-          map
+          <img src="map.png" className="size-auto max-w-[500px] max-h-[500px] object-contain"></img>
         </div>
       </div>
       <div className="group absolute bottom-4 right-4 bg-white drop-shadow p-2 rounded-full cursor-pointer">
