311d4497ba65849665d080b83f76f472274d9b76
mahutt <tommy.mahut@gmail.com>
2024-09-26 19:35:46 -0400

    Create input for locations

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,19 +1,28 @@
-import { Button } from "./components/ui/button"
-import { Input } from "./components/ui/input"
+import { Button } from './components/ui/button'
+import { LocationInput } from './components/location-input'
 import Chatbot from './components/chatbot'
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
 
 function Home() {
   return (
     <section className="flex-1 relative">
       <div className="max-w-screen-xl mx-auto px-16 h-full flex items-center justify-around gap-4">
-        <div className="w-[300px] flex flex-col gap-4 items-end">
-          <div className="text-4xl font-bold">Ship anything with Vüber</div>
-           <Input placeholder="From" className=""></Input>
-           <Input placeholder="To" className=""></Input>
-           <Button className="">Get Quote</Button>
+        <div className="w-[350px] flex flex-col gap-4 items-end">
+          <div className="text-6xl font-extrabold tracking-tight mb-8">
+            Ship anything with Vüber
+          </div>
+          <LocationInput placeholder="From" />
+          <LocationInput placeholder="To" filled={true} />
+          <Button>
+            Get Quote
+            <ArrowTopRightIcon className="w-4 h-4 ml-2" />
+          </Button>
         </div>
         <div>
-          <img src="map.png" className="size-auto max-w-[500px] max-h-[500px] object-contain"></img>
+          <img
+            src="map.png"
+            className="size-auto max-w-[500px] max-h-[500px] object-contain"
+          ></img>
         </div>
       </div>
       <Chatbot />
---
+++ client/src/components/location-input.tsx
@@ -0,0 +1,36 @@
+import { forwardRef, useState } from 'react'
+import { SewingPinIcon, SewingPinFilledIcon } from '@radix-ui/react-icons'
+
+export interface LocationInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  filled?: boolean
+}
+
+const LocationInput = forwardRef<HTMLInputElement, LocationInputProps>(
+  ({ className, type, filled, ...props }, ref) => {
+    const [focused, setFocused] = useState(false)
+    return (
+      <div
+        className={`w-full flex flex-row items-center gap-2 bg-slate-100 p-4 rounded-2xl transition-all duration-300 ${
+          focused ? 'ring-2 ring-primary-500' : ''
+        }`}
+      >
+        {filled ? (
+          <SewingPinFilledIcon className="w-4 h-4" />
+        ) : (
+          <SewingPinIcon className="w-4 h-4" />
+        )}
+        <input
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          className="outline-none ring-0 focus:ring-0 focus:outline-none bg-inherit"
+          type={type}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    )
+  }
+)
+
+export { LocationInput }
