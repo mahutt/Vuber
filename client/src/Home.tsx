dd26ba59dee21f6dbdf7d30df13dca2841a4f7b6
mahutt <tommy.mahut@gmail.com>
2024-10-22 11:58:09 -0400

    Force space between 'with' and 'Vuber'

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
       <div className="max-w-screen-xl mx-auto px-16 h-full flex items-center justify-around gap-4">
         <div className="w-[350px] flex flex-col gap-4 items-end">
           <div className="text-6xl font-extrabold tracking-tight mb-8">
-            Ship anything with
+            Ship anything with{' '}
             <span className="bg-gradient-to-tl from-blue-400 to-blue-200 bg-clip-text text-transparent">
               Vüber
             </span>
