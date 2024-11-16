c0693dcb99563197eabbd499d70cf96259a50732
mahutt <tommy.mahut@gmail.com>
2024-11-15 20:18:54 -0500

    Fix chris typos (stragy)

--- API/src/main/java/com/SOEN343/API/QuoteService/QuoteCalculator.java
+++ API/src/main/java/com/SOEN343/API/QuoteService/QuoteCalculator.java
@@ -6,26 +6,25 @@ import com.SOEN343.API.order.dto.ParcelDetailsDto;
 
 @Service
 public class QuoteCalculator {
-    
-    private IQuoteStrategy strategy;
-    private QuoteStragyFactory factory;
 
-    public QuoteCalculator(QuoteStragyFactory factory){
+    private IQuoteStrategy strategy;
+    private QuoteStrategyFactory factory;
+
+    public QuoteCalculator(QuoteStrategyFactory factory) {
         this.factory = factory;
     }
 
-    public void setSrategy(int numberOfOrders){
-        if(numberOfOrders<3){
+    public void setSrategy(int numberOfOrders) {
+        if (numberOfOrders < 3) {
             System.out.print("\n\n***BASIC***\n\n");
             strategy = factory.create("basic");
-        }
-        else{
+        } else {
             System.out.print("\n\n***DISCOUNT***\n\n");
             strategy = factory.create("discounted");
         }
     }
 
-    public double execute(ParcelDetailsDto[] parcels){
+    public double execute(ParcelDetailsDto[] parcels) {
         return this.strategy.calculateQuote(parcels);
     }
 }
--- API/src/main/java/com/SOEN343/API/QuoteService/QuoteStrategyFactory.java
+++ API/src/main/java/com/SOEN343/API/QuoteService/QuoteStrategyFactory.java
@@ -3,18 +3,15 @@ package com.SOEN343.API.QuoteService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class QuoteStragyFactory {
-    
-    public IQuoteStrategy create(String type){
-        if(type.equals("basic")){
+public class QuoteStrategyFactory {
+    public IQuoteStrategy create(String type) {
+        if (type.equals("basic")) {
             System.out.println("\n\nif");
             return new BasicQuoteService();
-        }
-        else if(type.equals("discounted")){
+        } else if (type.equals("discounted")) {
             System.out.println("\n\nelseif");
             return new DiscountedQuoteService();
-        }
-        else{
+        } else {
             System.out.println("\n\nelse");
             return new BasicQuoteService();
         }
