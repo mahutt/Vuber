877f2b217ef141d7837f23264d238ef2060f1167
mahutt <tommy.mahut@gmail.com>
2024-11-15 21:17:29 -0500

    Remove useless logs

--- API/src/main/java/com/SOEN343/API/QuoteService/QuoteStrategyFactory.java
+++ API/src/main/java/com/SOEN343/API/QuoteService/QuoteStrategyFactory.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 public class QuoteStrategyFactory {
     public IQuoteStrategy create(String type) {
         if (type.equals("basic")) {
-            System.out.println("\n\nif");
             return new BasicQuoteService();
         } else if (type.equals("discounted")) {
-            System.out.println("\n\nelseif");
             return new DiscountedQuoteService();
         } else {
-            System.out.println("\n\nelse");
             return new BasicQuoteService();
         }
     }
