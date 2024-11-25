4741d611d9012f6f8357486c15fd8ede136f9cb0
mahutt <tommy.mahut@gmail.com>
2024-11-24 21:59:38 -0500

    Disable alert emails temporarily

--- API/src/main/java/com/SOEN343/API/email/SmtpEmailFacade.java
+++ API/src/main/java/com/SOEN343/API/email/SmtpEmailFacade.java
@@ -7,20 +7,20 @@ public class SmtpEmailFacade {
         String to = "xvinivuberx@gmail.com";
         String from = "xvinivuberx@gmail.com";
         String host = "smtp.gmail.com";
-        emailer.sendEmail(details, to, from, host);
+        // emailer.sendEmail(details, to, from, host);
     }
 
     public void sendToSecurity(EmailDetails details) {
         String to = "tommy.mahut@gmail.com"; // security email
         String from = "xvinivuberx@gmail.com";
         String host = "smtp.gmail.com";
-        emailer.sendEmail(details, to, from, host);
+        // emailer.sendEmail(details, to, from, host);
     }
 
     public void sendToUser(EmailDetails details, String userEmail) {
         String to = userEmail;
         String from = "xvinivuberx@gmail.com";
         String host = "smtp.gmail.com";
-        emailer.sendEmail(details, to, from, host);
+        // emailer.sendEmail(details, to, from, host);
     }
 }
