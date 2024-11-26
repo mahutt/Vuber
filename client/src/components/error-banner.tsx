9b6ba27107ec4840bee3b9d6dfe3fb26a2729ac9
Ambrose Mclaughlin <mclaughlinambrose@gmail.com>
2024-11-25 19:43:16 -0500

    Cleared Client package

--- client/src/components/error-banner.tsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorBanner({ message }: { message: string }) {
-  return (
-    <div
-      className={`bg-red-500/20 border-2 border-red-500 rounded-lg text-red-500 p-4`}
-    >
-      {message}
-    </div>
-  )
-}
