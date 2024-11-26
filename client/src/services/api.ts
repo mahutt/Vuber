1e0fa4ee021c950d1c9cd294c8ab718dab56688a
mahutt <tommy.mahut@gmail.com>
2024-11-25 19:47:53 -0500

    Add base api url to .env

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+const baseURL = import.meta.env.VITE_BASE_API_URL || 'http://localhost:3001/api'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: baseURL,
   withCredentials: true,
 })
 
